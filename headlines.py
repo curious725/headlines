31998b71205530145c05a14465f3d5cb5be72af3
George Curious <curious725@gmail.com>
2016-05-20 18:40:05 +0300

    Retrieving the cookies for other data

--- headlines.py
+++ headlines.py
@@ -26,27 +26,27 @@ DEFAULTS = {'publication': 'bbc',
             }
 
 
+def get_value_with_fallback(key):
+    if request.args.get(key):
+        return request.args.get(key)
+    if request.cookies.get(key):
+        return request.cookies.get(key)
+    return DEFAULTS[key]
+
+
 @app.route("/")
 def home():
     # get customised headlines, based on user input or default
-    publication = request.args.get('publication')
-    if not publication:
-        publication = request.cookies.get("publication")
-        if not publication:
-            publication = DEFAULTS['publication']
+    publication = get_value_with_fallback("publication")
     articles = get_news(publication)
+
     # get customised weather based on user input or default
-    city = request.args.get('city')
-    if not city:
-        city = DEFAULTS['city']
+    city = get_value_with_fallback("city")
     weather = get_weather(city)
+
     # get customised currency based on user input or default
-    currency_from = request.args.get("currency_from")
-    if not currency_from:
-        currency_from = DEFAULTS['currency_from']
-    currency_to = request.args.get("currency_to")
-    if not currency_to:
-        currency_to = DEFAULTS['currency_to']
+    currency_from = get_value_with_fallback("currency_from")
+    currency_to = get_value_with_fallback("currency_to")
     rate, currencies = get_rate(currency_from, currency_to)
     response = make_response(render_template("home.html", articles=articles,
                                             weather=weather, currency_from=currency_from,
