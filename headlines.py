221d7577e6b607b5f6188f840121c618d77bc756
George Curious <curious725@gmail.com>
2016-05-20 14:27:53 +0300

    Add a function get_weather(), which will make a call to the weather API with a specific query

--- headlines.py
+++ headlines.py
@@ -17,6 +17,7 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 
 @app.route("/")
 def get_news():
+    # get customised headlines, based on user input or default
     query = request.args.get("publication")
     if not query or query.lower() not in RSS_FEEDS:
         publication = "bbc"
@@ -24,6 +25,22 @@ def get_news():
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
     return render_template("home.html",articles=feed['entries'])
+
+def get_weather(query):
+    api_url = "http://api.openweathermap.org/data/2.5/" +\
+        +"weather?q={}&units=metric&appid=814cd82f08404ebad86b7f7250d6a7c4"
+    query = urllib.quote(query)
+    url = api_url.format(query)
+    data = urllib2.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description":
+                    parsed["weather"][0]["description"],
+                    "temperature":parsed["main"]["temp"],
+                    "city":parsed["name"]
+                    }
+    return weather
 
 
 if __name__ == '__main__':
